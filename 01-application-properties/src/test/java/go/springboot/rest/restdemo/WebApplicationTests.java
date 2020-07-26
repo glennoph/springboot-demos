79f3a3f147f1f5cde6c8965ee5dca759e410f3f6
glenn opdycke-hansen <gopdyck@optum.com>
2020-07-26 11:05:48 -0500

    01-application-properties Comment the application.properties and use application.yml instead

--- 01-application-properties/src/test/java/go/springboot/rest/restdemo/WebApplicationTests.java
+++ 01-application-properties/src/test/java/go/springboot/rest/restdemo/WebApplicationTests.java
@@ -24,7 +24,7 @@ public class WebApplicationTests {
     public void expectDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("hello from application properties")));
+                .andExpect(content().string(containsString("hello from application yml")));
     }
 
 }
