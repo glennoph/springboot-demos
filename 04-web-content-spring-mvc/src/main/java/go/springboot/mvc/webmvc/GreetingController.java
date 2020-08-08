ca516977de6a4926d3bf0e1837e7a28add9b4d29
glenn opdycke-hansen <glennoph@gmail.com>
2020-08-08 16:50:36 -0500

    04-web-content-spring-mvc complete with tests

---
+++ 04-web-content-spring-mvc/src/main/java/go/springboot/mvc/webmvc/GreetingController.java
@@ -0,0 +1,17 @@
+package go.springboot.mvc.webmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false,
+            defaultValue="world") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ 04-web-content-spring-mvc/src/test/java/go/springboot/mvc/webmvc/SmokeTest.java
@@ -0,0 +1,19 @@
+package go.springboot.mvc.webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private GreetingController greetingController;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(greetingController).isNotNull();
+    }
+}
