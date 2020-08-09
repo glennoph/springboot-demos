7cf16fc551bfd00825db1563d5866e7e3db0c625
glenn opdycke-hansen <glennoph@gmail.com>
2020-08-09 13:15:24 -0500

    06 jpa h2 rest repositories

---
+++ 06-jpa-h2-rest/src/main/java/go/springboot/jparest/jpah2rest/JpaH2RestApplication.java
@@ -0,0 +1,13 @@
+package go.springboot.jparest.jpah2rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaH2RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaH2RestApplication.class, args);
+	}
+
+}
---
+++ 06-jpa-h2-rest/src/test/java/go/springboot/jparest/jpah2rest/JpaH2RestApplicationTests.java
@@ -0,0 +1,13 @@
+package go.springboot.jparest.jpah2rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
