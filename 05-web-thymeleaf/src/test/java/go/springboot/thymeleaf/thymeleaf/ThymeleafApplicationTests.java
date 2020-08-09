1e91f5c08a58ef5c16e67eb87870dc78c0edaf8d
glenn opdycke-hansen <glennoph@gmail.com>
2020-08-09 10:01:56 -0500

    05-web-thymeleaf

---
+++ 05-web-thymeleaf/src/test/java/go/springboot/thymeleaf/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package go.springboot.thymeleaf.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
